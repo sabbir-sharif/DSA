1f0a0b30680db64d1597dafc925354e96babd66e
Sabbir Sharif <sabbirshariff1996@gmail.com>
2026-02-02 23:43:01 +0600

    Binary Search & Reverse in Array

--- Array/NewArray.java
+++ Array/NewArray.java
@@ -30,6 +30,45 @@ public class NewArray {
         }
         return largestNumber;
     }
+    //Binary Search
+    public static int binarySearch(int arr[], int key){
+        //Define start and end index
+        int start = 0, end = arr.length -1;
+        //Compare
+        while(start <= end){
+            //define mid
+            int mid = (start + end) / 2;
+            //found
+            if(arr[mid] == key){
+                return mid;
+            }
+            if(arr[mid] < key){
+                start = mid + 1;
+            }
+            else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+
+    }
+    //Reverse in Array
+    public static void reverseInArray(int arr[]){
+        int left = 0, right = arr.length - 1;
+        int temp;
+        while(left <= right){
+            temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
 
 
     public static void main(String[] args) {
@@ -45,7 +84,7 @@ public class NewArray {
         //System.out.println();
 
         //dataType arrayName = {v1, v2, ...}
-        int myArray_2[] = {6, 7, 14, 45, 10};
+        int myArray_2[] = {2, 4, 6, 8, 10, 12, 14, 15};
 
         for (int i = 0; i < 5; i++) {
             //System.out.print(myArray_2[i] + " ");
@@ -71,6 +110,19 @@ public class NewArray {
         }
 
         //Largest Number
-        System.out.println("Largest number: " + largestNumber(myArray_2));
+        //System.out.println("Largest number: " + largestNumber(myArray_2));
+
+        //Binary Search
+        int value_binary = 12;
+        int found = binarySearch(myArray_2, value_binary);
+        if(found == -1){
+            //System.out.println(value_binary + " doesn't exist");
+        }
+        else {
+            //System.out.println(value_binary + " is in index: " + found);
+        }
+
+        //Reverse In Array
+        reverseInArray(myArray_2);
     }
 }
--- Array/note.md
+++ Array/note.md
@@ -18,3 +18,27 @@ Complexity
 - if greater then change the largest value with the index value.
 - compare with every index of the array.
 - finally return the largest value.
+
+### Day - 03
+1. Binary Search
+- Define start and end index
+- loop when start <= end
+- define mid in every iteration
+- if the mid == key, return mid
+- if mid < key, then start = mid + 1
+- if mid > key, then end = mid - 1
+- if key doenst exist, return -1 
+
+Complexity
+- Time complexity: O(log n) log 2 base n
+- Space complexity: O(n)
+
+Note
+- Array should be sorted
+
+2. Reverse in Array
+- Reverse an array without 2nd array(space complexity will be O(n))
+- Define left = 0, right = n-1
+- while left <= right
+- swap indexed values
+- left++, right--
