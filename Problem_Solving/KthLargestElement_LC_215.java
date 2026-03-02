04bddd9d87469817e959e7292a4f6e32c10669cb
sabbir-sharif <sabbirshariff1996@gmail.com>
2026-03-02 23:47:56 +0600

    add explaination to lc_215

--- Problem_Solving/KthLargestElement_LC_215.java
+++ Problem_Solving/KthLargestElement_LC_215.java
@@ -3,6 +3,34 @@ package Problem_Solving;
 import java.util.Arrays;
 import java.util.PriorityQueue;
 
+//Problem: K-th largest element
+//Platform: Leetcode_215
+//Difficulty: Medium
+
+//Problem understanding:
+// - Find the k-th largest element from the given array.
+// - Duplicate value is included. eg. 6, 6, 5
+// - 2nd Largest is 6 not 5.
+
+//Brute force:
+//Sorting the array. then find the k number value from last
+//Time: O(n log n)
+//Space: O(1)
+
+//Optimal:
+//Using Min Heap. if size is greather than k remove the tov value.
+//so the top value would be k-th largest element.
+//One value add. position change - log n
+//for n element - n log n
+//Time: O(n log k)
+//Heap is efficient when k << n
+
+//What i learned:
+//- Top k elements
+//- K-th largest
+//- K most frequent
+//→ Think Heap immediately.
+
 public class KthLargestElement_LC_215 {
     //Time: O(n log n)
     //Space: O(1)
