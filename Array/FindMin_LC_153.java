9149f974337bd3c9120f613c6f615f8ece0948ff
sabbir-sharif <sabbirshariff1996@gmail.com>
2026-03-09 23:47:44 +0600

    lc_153 optimal with binary search

--- Array/FindMin_LC_153.java
+++ Array/FindMin_LC_153.java
@@ -1,7 +1,7 @@
 package Array;
 
 public class FindMin_LC_153 {
-    public static int findMin(int arr[]){
+    public static int findMin_naive(int arr[]){
         int min = Integer.MAX_VALUE;
 
         for (int i = 0; i < arr.length; i++) {
@@ -12,9 +12,27 @@ public class FindMin_LC_153 {
 
         return min;
     }
+    public static int findMin(int arr[]){
+        int right = arr.length-1;
+        int left = 0;
+
+        while(left < right){
+            int mid = (left + right)/2;
+
+            if (arr[mid] > arr[right]){
+                left = mid +1;
+            }
+            else{
+                right = mid;
+            }
+        }
+        return arr[left];
+    }
+
     public static void main(String[] args) {
         int arr[] = {3,4,5,1,2};
 
-        findMin(arr);
+        int result = findMin(arr);
+        System.out.println(result);
     }
 }
