f15df9e91ce02894de15d566a26cf548aac29810
sabbir-sharif <sabbirshariff1996@gmail.com>
2026-03-16 22:51:15 +0600

    product except self lc_238

---
+++ Array/ProductExceptSeltf_LC_238.java
@@ -0,0 +1,57 @@
+package Array;
+
+public class ProductExceptSeltf_LC_238 {
+    //Approach: calculate total product non-0 elements
+    //check how many zeros are there
+    //if total_zero > 1 -> all value will be 0
+    //else_if total_zero == 1 -> 0 value index will be total product else all 0
+    //else product[i] = total product / arr[i]
+
+    //Time Complexity: O(n)
+    //Space : O(n)
+
+    //Issue: Used division here. some problem restrict using division
+    //Solution: use prefix & suffix product
+    public static int[] productExceptSelfr(int arr[]){
+        int product[] = new int[arr.length];
+        int prod = 1;
+        int count_zero = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] != 0){
+                prod *= arr[i];
+            }
+            if(arr[i] == 0){
+                count_zero++;
+            }
+        }
+        if(count_zero > 1){
+            for (int i = 0; i < arr.length; i++) {
+                product[i] = 0;
+            }
+        }
+        else if(count_zero == 1){
+            for (int i = 0; i < arr.length; i++) {
+                if(arr[i] == 0){
+                    product[i] = prod;
+                }
+                else {
+                    product[i] = 0;
+                }
+            }
+        }
+        else {
+            for (int i = 0; i < arr.length; i++) {
+                product[i] = prod / arr[i];
+            }
+        }
+//        for (int i = 0; i < arr.length; i++) {
+//            System.out.print(product[i] + " ");
+//        }
+        return product;
+    }
+    public static void main(String[] args) {
+        int arr[] = {-1,1,0,-3,3};
+
+        productExceptSelfr(arr);
+    }
+}
--- Array/min_max_in_array_gfg.java
+++ Array/min_max_in_array_gfg.java
@@ -3,7 +3,7 @@ package Problem_Solving;
 import java.util.*;
 
 public class min_max_in_array_gfg {
-    public static ArrayList<Integer> minMaxFromArray(int arr[]){
+    public static ArrayList<Integer> minMaxFromArray_normal(int arr[]){
         ArrayList<Integer> result = new ArrayList<>();
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
@@ -11,6 +11,52 @@ public class min_max_in_array_gfg {
         for(int n: arr){
             min = Math.min(min, n);
             max = Math.max(max, n);
+        }
+        result.add(min);
+        result.add(max);
+
+        return result;
+    }
+    // Optimal
+    public static ArrayList<Integer> minMaxFromArray(int arr[]){
+        ArrayList<Integer> result = new ArrayList<>();
+
+        int min, max, i;
+
+        if(arr.length % 2 == 0){
+            if(arr[0] < arr[1]){
+                min = arr[0];
+                max = arr[1];
+            }
+            else {
+                min = arr[1];
+                max = arr[0];
+            }
+            i = 2;
+        }
+        else {
+            min = max = arr[0];
+            i = 1;
+        }
+
+        while(i < arr.length-1){
+            if(arr[i] < arr[i+1]){
+                if(arr[i] < min){
+                    min = arr[i];
+                }
+                if(arr[i+1] > max){
+                    max = arr[i+1];
+                }
+            }
+            else {
+                if(arr[i+1] < min){
+                    min = arr[i+1];
+                }
+                if(arr[i] > max){
+                    max = arr[i];
+                }
+            }
+            i += 2;
         }
         result.add(min);
         result.add(max);
